023b03f1e8d8252b712dbb6ac07fb19b6cf1c969
Kyle Nguyen <trungkien7192@gmail.com>
2025-03-25 16:13:56 +0000

    //init the first project space for Hoa Sen Crowdfunding

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
+import React from 'react';
 import './App.css';
+import { Button, Typography, AppBar, Toolbar } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
+    <div className='App'>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            React + MUI App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <header className='App-header'>
+        <Typography variant='h4' gutterBottom>
+          GitHub Codespaces <span className='heart'>♥️</span> React
+        </Typography>
+        <Typography variant='body2' className='small'>
           Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        </Typography>
+        <Button variant='contained' color='primary' href='https://reactjs.org' target='_blank'>
+          Learn React
+        </Button>
       </header>
     </div>
   );
